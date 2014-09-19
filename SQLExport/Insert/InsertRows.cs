56b44ab8223c04f223669ec6b5e69357e3515953
fabio_leardini <fabio.leardini@unear.net>
2014-09-19 12:22:15 -0300

    first commit

---
+++ SQLExport/Insert/InsertRows.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLExport.Insert
+{
+    class InsertRows
+    {
+        public void insertRows(DataTable dt, StringBuilder values, OleDbConnection excel)
+        {
+            OleDbCommand excelCommand;
+
+            try
+            {
+                for (int row = 0; row < dt.Rows.Count; row++)
+                {
+                    values.Append("INSERT INTO [Plan1$] (");
+
+                    for (int col = 0; col < dt.Columns.Count; col++)
+                    {
+                        values.Append(col);
+                        values.Append(", ");
+                    }
+
+                    values.Remove(values.Length - 2, 2);
+                    values.Append(")");
+
+                    values.Append(" VALUES (");
+
+                    for (int col = 0; col < dt.Columns.Count; col++)
+                    {
+                        values.Append("'");
+                        values.Append(dt.Rows[row].ItemArray[col].ToString());
+                        values.Append("'");
+                        values.Append(", ");
+                    }
+
+                    values.Remove(values.Length - 2, 2);
+                    values.Append(")");
+
+                    //Abre conexão com a planilha Excel
+                    if (excel.State != ConnectionState.Open) excel.Open();
+
+                    //Executa a inserção na planilha
+                    excelCommand = new OleDbCommand(values.ToString(), excel);
+                    excelCommand.ExecuteNonQuery();
+
+                    //Limpa a string utilizada como comando de inserção
+                    values.Clear();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Erro ao inserir na planilha informações retornadas pelo banco de dados: " + ex.Message);
+            }
+        }
+    }
+}
